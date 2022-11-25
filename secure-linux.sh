85b99e32fa268300331d72c560be21a8df1d8b86
cyrus <mdoaie@yahoo.co.uk>
2022-11-25 19:52:27 +0000

    ntp added press_enter_continue

--- secure-linux.sh
+++ secure-linux.sh
@@ -525,35 +525,34 @@ function disable_passauth() {
 ################
 
 function set_ntp_client(){
-        @info_message_box "NTP Client"
-        echo -e -n "${lightcyan}"
-        echo -e " Many security protocols leverage the time."
-        echo -e " If your system time is incorrect, it could have negative impacts to your server."
-        echo -e " An NTP client can solve that problem by keeping your system time in-sync with global NTP servers."
-        echo -e -n "${nocolor}"
+    @info_message_box "NTP Client"
+    echo -e -n "${lightcyan}"
+    echo -e " Many security protocols leverage the time."
+    echo -e " If your system time is incorrect, it could have negative impacts to your server."
+    echo -e " An NTP client can solve that problem by keeping your system time in-sync with global NTP servers."
+    echo -e -n "${nocolor}"
 
-        if @confirm "Would you like setup NTP client?" ;then
-            echo -e 'apt-get install ntp'  | tee -a "$LOGFILE"
-            apt-get install ntp -y >> $LOGFILE 2>&1
-            if [ -e /etc/ntp.conf ]
-            then
-                @backup_file /etc/ntp.conf
+    if @confirm "Would you like setup NTP client?" ;then
+        echo -e 'apt-get install ntp'  | tee -a "$LOGFILE"
+        apt-get install ntp -y >> $LOGFILE 2>&1
+        if [ -e /etc/ntp.conf ]
+        then
+            @backup_file /etc/ntp.conf
 
-                sed -i -r -e "s/^((server|pool).*)/# \1         # commented by $(whoami) on $(date +"%Y-%m-%d @ %H:%M:%S")/" /etc/ntp.conf
-                echo -e "\npool pool.ntp.org iburst         # added by $(whoami) on $(date +"%Y-%m-%d @ %H:%M:%S")" | sudo tee -a /etc/ntp.conf
+            sed -i -r -e "s/^((server|pool).*)/# \1         # commented by $(whoami) on $(date +"%Y-%m-%d @ %H:%M:%S")/" /etc/ntp.conf
+            echo -e "\npool pool.ntp.org iburst         # added by $(whoami) on $(date +"%Y-%m-%d @ %H:%M:%S")" | sudo tee -a /etc/ntp.conf
 
-                service ntp restart
-                service ntp status | cat
-                @spinner 4
-                @sucess_message_box "NTP Client Installed."
-                NTP_INSTALLED='yes'
-            else
-                @error_message_box "Unable to locate /etc/ntp.conf file make sure ntp installed."
-                @press_enter_continue
-            fi
-        @spinner 2
+            service ntp restart
+            @info_message "NTP Status"
+            service ntp status | cat
+            @press_enter_continue
+            @sucess_message_box "NTP Client Installed."
+            NTP_INSTALLED='yes'
+        else
+            @error_message_box "Unable to locate /etc/ntp.conf file make sure ntp installed."
+        fi
     fi
-    @spinner 2
+
     clear
 }
 
@@ -928,7 +927,6 @@ function install_complete() {
     if @has_duplicate "/etc/ssh/sshd_config" ; then
         exit
     fi
-
 
     @warning_message_box "Seeing Ports Your Server Is Listening On:"
     ss -lntup
